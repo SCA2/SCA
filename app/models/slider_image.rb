d66b4cb23c30f66c0d00caebf3b76183f86e5c52
Tim Ryan <tpryan_1965@yahoo.com>
2014-02-22 12:05:39 -0800

    Fix-up nav

--- app/models/slider_image.rb
+++ app/models/slider_image.rb
@@ -1,5 +1,22 @@
 class SliderImage < ActiveRecord::Base
   
   validates :name, :caption, :url, presence: true
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |image|
+        csv << faq.attributes.values_at(*column_names)
+      end
+    end
+  end
+  
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      faq = find_by_id(row['id']) || new
+      faq.attributes = row.to_hash.slice(*accessible_attributes)
+      faq.save!
+    end
+  end
   
 end
