31fc8ed9c0982f8f240ed05674d9106b43497841
Tim Ryan <tpryan_1965@yahoo.com>
2014-02-23 15:34:36 -0800

    Add features controller

---
+++ spec/views/features/edit.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "features/edit" do
+  before(:each) do
+    @feature = assign(:feature, stub_model(Feature))
+  end
+
+  it "renders the edit feature form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", feature_path(@feature), "post" do
+    end
+  end
+end
---
+++ spec/views/features/index.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "features/index" do
+  before(:each) do
+    assign(:features, [
+      stub_model(Feature),
+      stub_model(Feature)
+    ])
+  end
+
+  it "renders a list of features" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+  end
+end
