f46524ca67f2cf26f547e9f7956585a6e1d37bd9
SCA2 <tpryan_1965@yahoo.com>
2014-11-14 08:15:12 -0800

    update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,7 @@ describe User do
   # end
   
   describe "#send_password_reset" do
-    let(:user) { FactoryGirl.create(:user) } 
+    let(:user) { create(:user) } 
     it "generates a unique password_reset_token each time" do
       user.send_password_reset
       last_token = user.password_reset_token
