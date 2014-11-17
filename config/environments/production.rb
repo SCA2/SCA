3b3a44844e6df137b51c31ef2e9084e5e6b682dc
SCA2 <tpryan_1965@yahoo.com>
2014-11-16 18:10:48 -0800

    update colors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,10 @@ SCA::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( search.js )
+
+  # Precompile for Foundation.
+  config.assets.precompile += %w( vendor/modernizr.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
