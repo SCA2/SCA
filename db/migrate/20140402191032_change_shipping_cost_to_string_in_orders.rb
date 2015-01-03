829eaadbf4c2612675fe09b52342551271aea494
SCA2 <tpryan_1965@yahoo.com>
2015-01-02 23:39:44 -0800

    remove bad migrations

--- db/migrate/20140402191032_change_shipping_cost_to_string_in_orders.rb
+++
@@ -1,5 +0,0 @@
-class ChangeShippingCostToStringInOrders < ActiveRecord::Migration
-  def change
-    change_column :orders, :shipping_cost, :string
-  end
-end
--- db/migrate/20140404020250_change_shipping_cost_to_integer_in_orders.rb
+++
@@ -1,5 +0,0 @@
-class ChangeShippingCostToIntegerInOrders < ActiveRecord::Migration
-  def change
-    change_column :orders, :shipping_cost, :integer
-  end
-end
