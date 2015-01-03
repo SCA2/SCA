a6af93b291673c2faae0bc72d1d7b7744de8095d
SCA2 <tpryan_1965@yahoo.com>
2015-01-02 23:28:58 -0800

    delete bad migration

--- db/migrate/20140218235052_rename_faqs_group_to_faqs_category.rb
+++
@@ -1,4 +0,0 @@
-class RenameFaqsGroupToFaqsCategory < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20140218235646_rename_group_to_category_2.rb
+++
@@ -1,7 +0,0 @@
-class RenameGroupToCategory2 < ActiveRecord::Migration
-  def change
-    change_table :faqs do |t|
-      t.rename :group, :category
-    end
-  end
-end
