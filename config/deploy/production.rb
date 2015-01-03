942ff039ee0b8f15b38db89b91b4b258921e1731
SCA2 <tpryan_1965@yahoo.com>
2015-01-03 11:11:50 -0800

    edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,9 +6,9 @@
 
 set :stage, :production
 
-role :app, %w{deploy@104.236.136.58}
-role :web, %w{deploy@104.236.136.58}
-role :db,  %w{deploy@104.236.136.58}
+# role :app, %w{deploy@104.236.136.58}
+# role :web, %w{deploy@104.236.136.58}
+# role :db,  %w{deploy@104.236.136.58}
 
 
 # Extended Server Syntax
@@ -18,7 +18,7 @@ role :db,  %w{deploy@104.236.136.58}
 # used to set extended properties on the server.
 
 # server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-
+server '104.236.136.58', user: 'deploy', roles: %w{web app}
 
 # Custom SSH Options
 # ==================
