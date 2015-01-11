8361952694ca91230e1a16ac3dded5eecd0e681c
SCA2 <tpryan_1965@yahoo.com>
2015-01-11 11:27:58 -0800

    testing paypal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,17 @@ SCA::Application.configure do
   
 #Paypal
 config.after_initialize do
-  ActiveMerchant::Billing::Base.mode = :production
+  # ActiveMerchant::Billing::Base.mode = :production
+  # paypal_options = {
+  #   login:      ENV['PAYPAL_PRO_LOGIN'],
+  #   password:   ENV['PAYPAL_PRO_PASSWORD'],
+  #   signature:  ENV['PAYPAL_PRO_SIGNATURE']
+  # }
+  ActiveMerchant::Billing::Base.mode = :test
   paypal_options = {
-    login:      ENV['PAYPAL_PRO_LOGIN'],
-    password:   ENV['PAYPAL_PRO_PASSWORD'],
-    signature:  ENV['PAYPAL_PRO_SIGNATURE']
+    login:      ENV['PAYPAL_DEV_LOGIN'],
+    password:   ENV['PAYPAL_DEV_PASSWORD'],
+    signature:  ENV['PAYPAL_DEV_SIGNATURE']
   }
   ::STANDARD_GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(paypal_options)
   ::EXPRESS_GATEWAY = ActiveMerchant::Billing::PaypalExpressGateway.new(paypal_options)
