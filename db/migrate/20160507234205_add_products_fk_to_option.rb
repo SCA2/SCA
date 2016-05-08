ce5103a4acc6c8625a985b19928b96e06eb484ad
SCA2 <tpryan_1965@yahoo.com>
2016-05-08 13:35:27 -0700

    fix name error

--- db/migrate/20160507234205_add_products_fk_to_option.rb
+++ db/migrate/20160507234205_add_products_fk_to_option.rb
@@ -1,4 +1,4 @@
-class AddLineItemFkToOption < ActiveRecord::Migration
+class AddProductsFkToOption < ActiveRecord::Migration
   def change
     add_foreign_key :options, :products, name: 'options_product_fk', on_delete: :cascade
   end
