bb40def26c76500c83c06809b90093b2c500193f
SCA2 <tpryan_1965@yahoo.com>
2016-07-29 19:12:47 -0700

    add test for TermsValidator

--- spec/services/terms_validator_spec.rb
+++ spec/services/terms_validator_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 describe TermsValidator do
 
   it { should respond_to(:accept_terms) }
-  # it { should respond_to(:validate_order) }
-  # it { should respond_to(:validate_terms) }
+
+  it 'is valid with valid params' do
+    params = { accept_terms: '1'}
+    expect(TermsValidator.new(params)).to be_valid
+  end
+
+  it 'is not valid with invalid params' do
+    params = { accept_terms: '0'}
+    expect(TermsValidator.new(params)).to_not be_valid
+  end
 
 end
