86e8ae1ffea47e40bddafffcb8388c674f0fa79c
SCA2 <tpryan_1965@yahoo.com>
2017-04-01 11:18:16 -0700

    add stripe_token to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905195050) do
+ActiveRecord::Schema.define(version: 20170401181555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 20160905195050) do
     t.integer  "sales_tax"
     t.string   "use_billing",      limit: 255
     t.string   "ip_address"
+    t.string   "stripe_token"
   end
 
   add_index "orders", ["cart_id"], name: "index_orders_on_cart_id", using: :btree
