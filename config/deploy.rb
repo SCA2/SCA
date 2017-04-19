0d1bcfcf90dab68274ac2e5659dd5969e2ceb406
SCA2 <tpryan_1965@yahoo.com>
2017-04-18 21:39:59 -0700

    fixup deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,9 +11,6 @@ set :branch, 'master'
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/deploy/SCA'
 
-# Default value for :scm is :git
-set :scm, :git
-
 # Default value for :format is :pretty
 # set :format, :pretty
 
