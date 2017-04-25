2164812d7cf17c0e0e202b57b875f6ecbf55ee48
SCA2 <tpryan_1965@yahoo.com>
2017-04-25 14:12:29 -0700

    trying to fix email urls 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ SCA::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { protocol: 'https', host: ENV['MAILER_HOST'] }
-  # config.action_mailer.default_url_options = { host: 'https://seventhcircleaudio.com' }
+  # config.action_mailer.default_url_options = { protocol: 'https', host: ENV['MAILER_HOST'] }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'seventhcircleaudio.com' }
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILER_ADDRESS'],
     port:                 ENV['MAILER_PORT'],
