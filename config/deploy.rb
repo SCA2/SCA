95a3f9cb6801b30f300b7f4660aff06808035b57
SCA2 <tpryan_1965@yahoo.com>
2017-04-25 14:34:59 -0700

    add application.yml scp task to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,25 @@ set :bundle_binstubs, nil
 # Default value for keep_releases is 5
 set :keep_releases, 3
 
+namespace :figaro do
+  desc "SCP transfer figaro configuration to the shared folder"
+  task :setup do
+    on roles(:app) do
+      upload! "config/application.yml", "#{shared_path}/application.yml", via: :scp
+    end
+  end
+
+  desc "Symlink application.yml to the release path"
+  task :symlink do
+    on roles(:app) do
+      execute "ln -sf #{shared_path}/application.yml #{release_path}/config/application.yml"
+    end
+  end
+end
+
+after "deploy:started", "figaro:setup"
+after 'deploy:updating', 'figaro:symlink'
+
 namespace :deploy do
 
   desc 'Restart application'
